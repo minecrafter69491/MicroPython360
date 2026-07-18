587b3cbc1eb54f98435f94b7dca8f8a49e9c3281
PyOS Dev <jmayor6672@gmail.com>
2026-07-18 19:35:24 +0100

    Add files via upload

---
+++ MicroPython360/py/genhdr/moduledefs.h
@@ -0,0 +1,9 @@
+// This file was automatically generated
+MP_REGISTER_MODULE(MP_QSTR_builtins, mp_module_builtins)
+MP_REGISTER_MODULE(MP_QSTR_cmath, mp_module_cmath)
+MP_REGISTER_MODULE(MP_QSTR_gc, mp_module_gc)
+MP_REGISTER_MODULE(MP_QSTR_math, mp_module_math)
+MP_REGISTER_MODULE(MP_QSTR_micropython, mp_module_micropython)
+MP_REGISTER_MODULE(MP_QSTR_sys, mp_module_sys)
+MP_REGISTER_MODULE(MP_QSTR__thread, mp_module_thread)
+MP_REGISTER_MODULE(MP_QSTR___main__, mp_module___main__)
---
+++ MicroPython360/py/genhdr/mpversion.h
@@ -0,0 +1,6 @@
+// This file was automatically generated
+#define MICROPY_VERSION_MAJOR (1)
+#define MICROPY_VERSION_MINOR (23)
+#define MICROPY_VERSION_MICRO (0)
+#define MICROPY_GIT_TAG ""
+#define MICROPY_GIT_HASH ""
